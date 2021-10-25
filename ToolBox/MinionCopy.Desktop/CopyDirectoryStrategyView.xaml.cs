6343cf8dbd7e4de03c7bced8b0b850ec60c2bbc2
diamtr <67097638+diamtr@users.noreply.github.com>
2021-10-25 21:34:16 +0300

    Allow drop files/directories into copy item Source/Destination

--- ToolBox/MinionCopy.Desktop/CopyDirectoryStrategyView.xaml.cs
+++ ToolBox/MinionCopy.Desktop/CopyDirectoryStrategyView.xaml.cs
@@ -22,5 +22,31 @@ namespace MinionCopy.Desktop
     {
       InitializeComponent();
     }
+
+    private void TextBox_Drop(object sender, DragEventArgs e)
+    {
+      // https://stackoverflow.com/a/45946696/8878639
+      var textBox = (TextBox)sender;
+      if (!e.Data.GetDataPresent(DataFormats.FileDrop) ||
+          textBox == null)
+        return;
+
+      var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+      if (files == null || files.Length == 0)
+        return;
+      textBox.SetCurrentValue(TextBox.TextProperty, files[0]);
+      var bindingExpression = textBox.GetBindingExpression(TextBox.TextProperty);
+      if (bindingExpression != null && bindingExpression.ParentBinding != null &&
+          (bindingExpression.ParentBinding.Mode == BindingMode.Default ||
+           bindingExpression.ParentBinding.Mode == BindingMode.TwoWay ||
+           bindingExpression.ParentBinding.Mode == BindingMode.OneWayToSource))
+        bindingExpression.UpdateSource();
+    }
+
+    private void TextBox_PreviewDragOver(object sender, DragEventArgs e)
+    {
+      // https://stackoverflow.com/a/37415941/8878639
+      e.Handled = true;
+    }
   }
 }
